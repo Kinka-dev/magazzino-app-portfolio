395914ad325b5c030b32394fee640781e88fc283
Kinka-dev <karinaamanda.m@gmail.com>
2026-01-23 14:41:05 +0100

    Initial commit: progetto Spring Boot per gestione magazzino generato da Initializr

---
+++ src/main/java/com/karina/gestione_magazzino/GestioneMagazzinoApplication.java
@@ -0,0 +1,13 @@
+package com.karina.gestione_magazzino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneMagazzinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneMagazzinoApplication.class, args);
+	}
+
+}
